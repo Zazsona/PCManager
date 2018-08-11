a507ee0e29bc7c78f18a53d4373e255db88f312e
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-08-11 21:04:40 +0100

    Refactored desktop code
    This commit mainly focuses on making the code much tidier. However, also prepares for some new features to be added to the mobile side.
    
    - Code repositioned
    - Implemented a more thorough feedback system for when commands are received
    - Command/feedback values are now represented as constants
    
    Preparation for broadcast system:
    - Added the ability to modify the port
    - Removed IP code generation, as broadcast detection means the user won't directly handle IPs

--- PCManager - Desktop/src/main/java/Start.java
+++ PCManager - Desktop/src/main/java/Start.java
@@ -1,9 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,106 +8,108 @@ import javax.swing.JOptionPane;
 
 import com.google.gson.Gson;
 
-public class Start {
-	private static String clientID = "";
+public class Start
+{
+	private static File chFile = new File(System.getenv("AppData") + "\\.ZazDat\\PCManager\\connection-hash.txt");
+
+	private static short port = 2865;
 	private static ObjectOutputStream output;
 	private static ObjectInputStream input;
-	
+
+	private static final byte SHUTDOWN = 0;
+	private static final byte STANDBY = 1;
+	private static final byte LOCK = 2;
+	private static final byte REBOOT = 3;
+
+	private static final byte CONNECTION_ESTABLISHED = -1;
+	private static final byte COMMAND_EXECUTED_SUCCESSFULLY = -2;
+	private static final byte COMMAND_NOT_FOUND = -3;
+	private static final byte COMMAND_NOT_SUPPORTED_ON_OS = -4;
+
+
 	public static void main(String[] args) 
 	{
-		clientID = "0";
+		if (args.length > 0)
+		{
+			try
+			{
+				port = Short.parseShort(args[0]);
+				if (port < 0)
+				{
+					throw new NumberFormatException();
+				}
+			}
+			catch (NumberFormatException e)
+			{
+				System.err.println("The passed argument is not a valid port.");
+			}
+
+		}
 		try
 		{
-			ServerSocket server = new ServerSocket(2865, 1);
-		    String dir = System.getenv("AppData");
-		    dir += "\\.ZazDat\\PCManager";
-			File dirFile = new File(dir);
-			File idFile = new File(dir+"/id.txt");
-			if (!idFile.exists())
+			ServerSocket server = new ServerSocket(port, 1);
+			if (!chFile.exists())
 			{
-				try
-				{
-					dirFile.mkdir();
-					idFile.createNewFile();
-					InetAddress inetAddress = InetAddress.getLocalHost();
-					String encryptedData = getCodeFromIP(inetAddress.getHostAddress());
-					FileWriter fileWriter = new FileWriter(idFile);
-					PrintWriter printWriter = new PrintWriter(fileWriter);
-					printWriter.print(encryptedData);
-					printWriter.close();
-					fileWriter.close();
-					JOptionPane.showMessageDialog(null, "Welcome to PCManager! To set up, please enter this code in the mobile app and click \"Ok\":\n"+encryptedData);
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-					return;
-				}
+				firstTimeSetup();
 			}
-			Scanner fileScanner = new Scanner(idFile);
-			clientID = fileScanner.next();
-			String decryptedIP = getIPFromCode(clientID);
-			if (!decryptedIP.equals(InetAddress.getLocalHost().getHostAddress()))
-			{
-				String encryptedData = getCodeFromIP(InetAddress.getLocalHost().getHostAddress());
-				FileWriter fileWriter = new FileWriter(idFile);
-				PrintWriter printWriter = new PrintWriter(fileWriter);
-				printWriter.print(encryptedData);
-				printWriter.close();
-				fileWriter.close();
-				JOptionPane.showMessageDialog(null, "Your local IP address has changed. To avoid this, please set-up a static IP address.\nTo reconnect, please select \"New Code\" in the app and enter:\n"+encryptedData);
-			}
-			while (true)
-			{
-				try
-				{	
-					System.out.println("Ready.");
-					Socket connection = server.accept();
-					output = new ObjectOutputStream(connection.getOutputStream());
-					output.flush();	
-					input = new ObjectInputStream(connection.getInputStream());
-					try
-					{
-						output.writeObject("1337");
-						output.flush();
-						handleConnection(connection);
-					}
-					catch (IOException e)
-					{
-						//Fail silently
-					}
 
-					//Check if id file exists
-					//If not, launch setup();
-					//Else, wait for input
-				}
-				catch (IOException e)
-				{
-					System.out.println("Connection failed.");
-					e.printStackTrace();
-				}
+			String savedIP = getSavedIP();
+			if (!savedIP.equals(InetAddress.getLocalHost().getHostAddress()))
+			{
+				manageIPChange();
 			}
+
+			runServer(server);
 		}
 		catch (IOException e)
 		{
-			System.out.println("ERROR: Port is already taken.");
+			System.err.println("ERROR: Default port is already taken. You can pass a new port as a command line argument.");
+		}
+	}
+
+	private static void runServer(ServerSocket server)
+	{
+		while (true)
+		{
+			try
+			{
+				Socket connection = server.accept();
+				output = new ObjectOutputStream(connection.getOutputStream());
+				output.flush();
+				input = new ObjectInputStream(connection.getInputStream());
+				try
+				{
+					output.writeObject(CONNECTION_ESTABLISHED);
+					output.flush();
+					handleConnection(connection);
+				}
+				catch (IOException e)
+				{
+					//Fail silently
+				}
+			}
+			catch (IOException e)
+			{
+				System.out.println("Connection failed.");
+				e.printStackTrace();
+			}
 		}
 	}
 	private static void handleConnection(Socket connection)
 	{
 		while (!connection.isClosed())
 		{
-			try 
+			try
 			{
 				String receivedInput = (String) input.readObject();
-				CommandInterpreter(receivedInput);
+				manageCommand(decrypt(receivedInput));
 			}
 			catch (IOException | ClassNotFoundException e)
 			{
-				try 
+				try
 				{
 					connection.close();
-				} 
+				}
 				catch (IOException e1)
 				{
 					e1.printStackTrace();
@@ -120,62 +117,40 @@ public class Start {
 			}
 		}
 	}
-	private static String encrypt(String source)
+	private static void manageCommand(String request) //Possibly make system specific variations. Windows by default.
 	{
-		return source; //TODO: Implement again, but more secure
-	}
-	private static String decrypt(String source)
-	{
-		return source; //TODO: Implement again, but more secure
-	}
-	private static String getCodeFromIP(String source)
-	{
-		return source; //TODO: Implement again (with better system)
-		//The role of this will be to turn an IP address into a more user friendly string, which can then be entered onto the mobile device.
-	}
-	private static String getIPFromCode(String source)
-	{
-		return source;
-	}
+		try
+		{
+			Gson gson = new Gson();
+			ResponseData responseData = gson.fromJson(request, ResponseData.class); //Set object
 
-	private static void CommandInterpreter(String sentData)
-	{
-		Gson gson = new Gson();
-		ResponseData responseData = gson.fromJson(decrypt(sentData), ResponseData.class); //Set object
-		try
-		{
-			output.writeObject("-1");
-			output.flush();
-			manageCommand(responseData.cid); //If it is, execute the command
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	private static void manageCommand(String commandCode) //Possibly make system specific variations. Windows by default.
-	{
-		try
-		{
-			switch (commandCode)
+			switch (Integer.parseInt(responseData.cid))
 			{
-				case "0": //Shutdown
+				case SHUTDOWN: //Shutdown
 					Runtime.getRuntime().exec("shutdown -t 0");
+					output.writeObject(COMMAND_EXECUTED_SUCCESSFULLY);
+					output.flush();
 					break;
-				case "1": //Standby
+				case STANDBY: //Standby
 					Runtime.getRuntime().exec("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
+					output.writeObject(COMMAND_EXECUTED_SUCCESSFULLY);
+					output.flush();
 					break;
-				case "2": //Lock
+				case LOCK: //Lock
 					Runtime.getRuntime().exec("rundll32.exe user32.dll, LockWorkStation");
+					output.writeObject(COMMAND_EXECUTED_SUCCESSFULLY);
+					output.flush();
 					break;
-				case "3": //Reboot
+				case REBOOT: //Reboot
 					Runtime.getRuntime().exec("shutdown -r -t 0");
+					output.writeObject(COMMAND_EXECUTED_SUCCESSFULLY);
+					output.flush();
 					break;
-				case "#":
-					//output.writeObject("#");
-					//output.flush();
+				default:
+					output.writeObject(COMMAND_NOT_FOUND);
+					output.flush();
 					break;
+
 			}
 		}
 		catch (IOException e)
@@ -184,10 +159,64 @@ public class Start {
 			e.printStackTrace();
 		}
 	}
+
+	private static String getSavedIP() throws FileNotFoundException
+	{
+		Scanner fileScanner = new Scanner(chFile);
+		String storedIP = fileScanner.next();
+		return storedIP;
+	}
+	private static String updateSavedIP() throws IOException
+	{
+		String IP = InetAddress.getLocalHost().getHostAddress();
+		PrintWriter printWriter = new PrintWriter(chFile);
+		printWriter.print(IP);
+		printWriter.close();
+		return IP;
+	}
+
+	private static void firstTimeSetup()
+	{
+		try
+		{
+			chFile.getParentFile().mkdirs();
+			chFile.createNewFile();
+			String IP = updateSavedIP();
+			JOptionPane.showMessageDialog(null, "Welcome to PCManager! To set up, please enter this code in the mobile app and click \"Ok\":\n"+IP);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	private static void manageIPChange() throws IOException
+	{
+		String IP = updateSavedIP();
+		JOptionPane.showMessageDialog(null, "Your local IP address has changed. To avoid this, please set-up a static IP address.\nTo reconnect, please select \"New Code\" in the app and enter:\n"+IP);
+	}
+
+
+	private static String encrypt(String source)
+	{
+		return source; //TODO: Implement again, but more secure
+	}
+	private static String decrypt(String source)
+	{
+		return source; //TODO: Implement again, but more secure
+	}
+	
+
 	private class ResponseData //JSON class
 	{
-		String id;
-		String cid;
+		String ip; //The connection hash to identify the link (Basically, the desktop's IP)
+		String cid; //Command ID.
+
+		public ResponseData(String ip, String cid)
+		{
+			this.ip = ip;
+			this.cid = cid;
+		}
 	}
 
 }
